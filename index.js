c1d1b0b9f598d4db06c6255af55e841f3e2d717d
Chunsaeng20 <gabril1022@gmail.com>
2023-12-10 22:16:29 +0900

    Indicate today's date

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+let DATA={} ;
+
 document.addEventListener('DOMContentLoaded', () => {
     // 오늘 날짜 받아오기
     let today = new Date();   
+
+    const inputBox=document.querySelector('.input-box');
+    const inputBtn=document.querySelector('.input-btn');
+    const inputList=document.querySelector('.todoList');
+    let clickedDate;
 
     // 현재 날짜 받아오기
     let currDate  = new Date();
@@ -62,6 +69,13 @@ document.addEventListener('DOMContentLoaded', () => {
         document.querySelector('.info-day').textContent   = currDay;
         document.querySelector('#head-month').textContent = currMonth + ' ' + currYear;
         updateCalendar();
+
+        // 만약 To do list가 켜져 있으면 끈다
+        let todoList = document.querySelector('.todo-list');
+        if(todoList.style.visibility === 'visible')
+        {
+            document.querySelector('#close-btn').click();
+        }        
     });
 
     // -------------------------------------------------------------------------------------------------------
@@ -102,11 +116,16 @@ document.addEventListener('DOMContentLoaded', () => {
         else if (event.target.tagName === 'TD' && event.target.closest('#calendar')) {
             // 클릭된 날짜 받아오기
             let clickedDay = parseInt(event.target.textContent);
+            event.target.classList.add('active');
+            currDate = new Date(currDate.getFullYear(), currDate.getMonth(), event.target.innerHTML);
+            clickedDate = currDate.toLocaleDateString('ko-KR', {year: 'numeric', month: '2-digit', day: '2-digit'});
             // info-day, info-month 업데이트
             document.querySelector('.info-day').textContent   = clickedDay;
             document.querySelector('.info-month').textContent = currMonth;
             // 화면 바꾸기
             slideWindowRight();
+            // 클릭된 날짜의 Todo List로 업데이트
+            updateList();
             // Todo list 띄우기
             showOnTodolist();
         }
@@ -165,6 +184,102 @@ document.addEventListener('DOMContentLoaded', () => {
         // 화면 바꾸기
         slideWindowLeft();
     });
+
+    // -------------------------------------------------------------------------------------------------------
+
+    // Todo 삭제
+    function deleteTodo(E) {
+        E.preventDefault();
+        let delParentLi = E.target.parentNode;
+        inputList.removeChild(delParentLi);
+
+        if (!DATA[clickedDate] || !Array.isArray(DATA[clickedDate])) {
+            DATA[clickedDate] = [];
+        }
+
+        const cleanToDos = DATA[clickedDate].filter(function (todo) {
+            return todo.id !== parseInt(delParentLi.id);
+        });
+
+        DATA[clickedDate] = cleanToDos;
+        save();
+    }
+
+    // Todo list 인풋 입력 이벤트 리스너
+    inputBtn.addEventListener('click',function(E){ 
+        E.preventDefault();
+        let input = inputBox.value;
+        InsertTodo(input);
+    });
+
+    // Todo list에 삽입
+    function InsertTodo(text){
+        let todo = {
+            todo: text,
+        }
+
+        if(!DATA[clickedDate]) {
+            DATA[clickedDate] = [];
+            DATA[clickedDate].push(todo);
+        } else {
+            DATA[clickedDate].push(todo);
+        }
+
+        const listE     = document.createElement('li');
+        const spanE     = document.createElement('span');
+        const deleteBtn = document.createElement('button');
+
+        deleteBtn.setAttribute('class', 'del-data');
+        deleteBtn.innerText = "DEL";
+        spanE.innerHTML = text;
+        listE.appendChild(spanE);
+        listE.appendChild(deleteBtn);
+        inputList.appendChild(listE);
+
+        listE.setAttribute('id', DATA[clickedDate].length);
+        deleteBtn.addEventListener('click', deleteTodo);
+
+        todo.id = DATA[clickedDate].length;
+        save();
+        inputBox.value='';
+    }
+
+    // 선택된 날짜의 Todo List출력
+    function updateList(){ 
+        let savedE = localStorage.getItem(clickedDate);
+
+        let listE = document.querySelectorAll('LI');
+
+        for(let i = 0; i < listE.length; i++) {
+            inputList.removeChild(listE[i])
+        }
+
+        if ( savedE!==null ) {
+            const parsed=JSON.parse(localStorage.getItem(clickedDate));
+            parsed.forEach(function(Todo){
+                if(Todo){
+                    let li     = document.createElement('li');
+                    let sp     = document.createElement('span');
+                    let delbtn = document.createElement('button');
+
+                    delbtn.setAttribute('class', 'del-data');
+                    delbtn.innerText = "DEL";
+                    sp.innerHTML = Todo.todo;
+                    li.appendChild(sp);
+                    li.appendChild(delbtn);
+                    inputList.appendChild(li);
+
+                    li.setAttribute('id', Todo.id);
+                    delbtn.addEventListener('click', deleteTodo);
+                }
+            });
+        }
+    }
+
+    // 로컬 스토리지 저장
+    function save(){ 
+        localStorage.setItem(clickedDate, JSON.stringify(DATA[clickedDate]));
+    }
 
     // -------------------------------------------------------------------------------------------------------
 
