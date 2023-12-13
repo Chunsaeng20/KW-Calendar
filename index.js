a9bef882d92d7435177cb75d564fe87bf4fc2065
Chunsaeng20 <gabril1022@gmail.com>
2023-12-13 21:37:23 +0900

    Add function that show map

--- index.js
+++ index.js
@@ -307,6 +307,9 @@ document.addEventListener('DOMContentLoaded', () => {
             inputList.removeChild(listE[i])
         }
 
+        // Tips에서 지도 삭제
+        findPlace("1");
+
         if ( savedE !== null ) {
             const parsed = JSON.parse(localStorage.getItem(clickedDate));
             parsed.forEach(function(Todo){
@@ -324,6 +327,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
                     li.setAttribute('id', Todo.id);
                     delbtn.addEventListener('click', deleteTodo);
+                    
+                    // Tips에 지도 생성
+                    findPlace(Todo.todo);
 
                     // 문자열이 없으면 생성
                     if(!DATA[clickedDate]) {
@@ -366,6 +372,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // 일정이 저장된 날짜 불러오기
     function loadScheduleDate() {
+        // 배열 초기화
+        scheduleDate.splice(0, scheduleDate.length);
         let item = JSON.parse(localStorage.getItem("Date"));
         if(item){
             item.forEach((element)=>{
@@ -375,5 +383,83 @@ document.addEventListener('DOMContentLoaded', () => {
         // 로컬 스토리지에 날짜 저장
         localStorage.setItem("Date", JSON.stringify(scheduleDate));
     }
+
+    // -------------------------------------------------------------------------------------------------------
+
+    // 일정에서 장소 찾기
+    function findPlace(todo){
+        const place = ["화도관", "비마관", "옥의관", "중앙도서관", "복지관", "연구관", "참빛관", "연촌재", "한천재", "한울관", "승리관", "누리관", "새빛관", "빛솔재", "노천극장", "동해문화예술관", "80주년 기념관", "아이스링크", "광운대"];
+        // 일정에 존재하는 장소
+        let currPlace = "";
+
+        if(!todo) return;
+
+        // 일정에 장소가 들어있는지 확인
+        place.forEach((value)=>{
+            if(todo.includes(value) && currPlace === ""){
+                currPlace = value;
+            }
+        });
+
+        let map = document.querySelector('#map')
+        map.innerHTML = "";
+        // 일정에 장소가 들어있으면
+        if(currPlace === "화도관"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3160.222639320751!2d127.05456138852148!3d37.62045044891418!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbbeadac4e8b9%3A0x83a20eb4f21855d0!2z7ISc7Jq47Yq567OE7IucIOyblOqzhDHrj5kg6rSR7Jq064yA7ZWZ6rWQIO2ZlOuPhOq0gA!5e0!3m2!1sko!2skr!4v1702463570333!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "비마관"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3160.2580531117346!2d127.05730177629862!3d37.61961732108192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbbeacb4cfbf7%3A0x4fd43175e455c4d1!2z7ISc7Jq47Yq567OE7IucIOyblOqzhOuPmSDqtJHsmrTrjIDtlZnqtZAg67mE66eI6rSA!5e0!3m2!1sko!2skr!4v1702464482013!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "옥의관"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3160.2922949537688!2d127.0542280885211!3d37.61881174900933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbb9521ecd6e3%3A0xd5055206b7732228!2z7ISc7Jq47Yq567OE7IucIOyblOqzhDHrj5kg6rSR7Jq064yA7ZWZ6rWQIOyYpeydmOq0gA!5e0!3m2!1sko!2skr!4v1702464560969!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "중앙도서관"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1117.32090722732!2d127.05899625231243!3d37.61955361483271!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbb76dbe145ff%3A0xfd66f26178ce0d69!2z6rSR7Jq064yA7ZWZ6rWQIOykkeyVmeuPhOyEnOq0gA!5e0!3m2!1sko!2skr!4v1702464613606!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "복지관"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1580.122476336857!2d127.05757818887326!3d37.61992551752468!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbb94dc6a2833%3A0x31d57fa9e0666424!2z7ISc7Jq47Yq567OE7IucIOyblOqzhDHrj5kg6rSR7Jq064yA7ZWZ6rWQIOuzteyngOq0gA!5e0!3m2!1sko!2skr!4v1702464661645!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "연구관"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1580.1373366405057!2d127.05708943887329!3d37.619226317544666!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbb94c11a71d1%3A0x72d3894d13bed9ff!2z7ISc7Jq47Yq567OE7IucIOyblOqzhDHrj5kg6rSR7Jq064yA7ZWZ6rWQIOyXsOq1rOq0gA!5e0!3m2!1sko!2skr!4v1702464683015!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "참빛관"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d939.5497942453098!2d127.0594983994693!3d37.61966054675825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbb9552b79ad7%3A0xa353b2a95c2f83f2!2z7ISc7Jq47Yq567OE7IucIOyblOqzhDHrj5kg6rSR7Jq064yA7ZWZ6rWQIOywuOu5m-q0gA!5e0!3m2!1sko!2skr!4v1702465765176!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "연촌재"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3160.1636052586978!2d127.05399647629875!3d37.6218392209543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbbeb5db411ff%3A0xbdb9d725d53cbf3f!2z6rSR7Jq064yA7ZWZ6rWQIOyXsOy0jOyerA!5e0!3m2!1sko!2skr!4v1702465789622!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "한천재"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3160.2332630825667!2d127.0550241762988!3d37.620200521048346!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbb94ca3a7079%3A0x9cc2f2b37e432b2d!2z6rSR7Jq064yA7ZWZ6rWQIO2VnOyynOyerA!5e0!3m2!1sko!2skr!4v1702465806278!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "한울관"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3160.2108319046556!2d127.05441317629892!3d37.620728221018226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbbeb4cde60a1%3A0xdacff6df925558e9!2z6rSR7Jq064yA7ZWZ6rWQIO2VnOyauOq0gA!5e0!3m2!1sko!2skr!4v1702465822122!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "누리관"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3160.2104730531146!2d127.05441317617165!3d37.62073666305378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbb936ac3b12f%3A0xb90a5aa83e0116dd!2z6rSR7Jq064yAIOuIhOumrOq0gA!5e0!3m2!1sko!2skr!4v1702465860076!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "새빛관"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3160.2104730531146!2d127.05441317617165!3d37.62073666305378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbbeaaf919c85%3A0xbef57637167e129c!2z6rSR7Jq064yA7ZWZ6rWQIOyDiOu5m-q0gA!5e0!3m2!1sko!2skr!4v1702465873076!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "빛솔재"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1879.0658096828442!2d127.05527043928855!3d37.62099699805559!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbbeca86fafd5%3A0xac616b5898e6d7d9!2z6rSR7Jq064yA7ZWZ6rWQ6rO16rO16riw7IiZ7IKsKOycoCk!5e0!3m2!1sko!2skr!4v1702465903851!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "노천극장"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1117.3170957559892!2d127.05907404932606!3d37.619807232994745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbb952907ece3%3A0x15c12166c3c20e4a!2z6rSR7Jq064yA7ZWZ6rWQ!5e0!3m2!1sko!2skr!4v1702465948377!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "동해문화예술관"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1117.3170957559892!2d127.05907404932606!3d37.619807232994745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbb94b89cf307%3A0x6bce5ccde500f450!2z64-Z7ZW066y47ZmU7JiI7Iig6rSA!5e0!3m2!1sko!2skr!4v1702465960520!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "80주년 기념관"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d790.0621597558062!2d127.05888122665434!3d37.61983879393919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbb94dc983783%3A0x1e29cce379bed986!2z6rSR7Jq064yA7ZWZ6rWQ7Jqw7Y647Leo6riJ6rWt!5e0!3m2!1sko!2skr!4v1702466018904!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "아이스링크"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1580.1166957603248!2d127.05473454916594!3d37.62019749948702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbbde6e8d5f73%3A0x33619639d65ad4f5!2z6rSR7Jq064yA7ZWZ6rWQIOyVhOydtOyKpOunge2BrOyepQ!5e0!3m2!1sko!2skr!4v1702465981963!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+        else if(currPlace === "광운대"){
+            map.innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d790.0621597558062!2d127.05888122665434!3d37.61983879393919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357cbb952907ece3%3A0x15c12166c3c20e4a!2z6rSR7Jq064yA7ZWZ6rWQ!5e0!3m2!1sko!2skr!4v1702466070675!5m2!1sko!2skr" width="200" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
+        }
+    }
+
+    // -------------------------------------------------------------------------------------------------------
 
 });
